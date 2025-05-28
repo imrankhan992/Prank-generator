e54bc8782a23ebc4ed62a02f0ce27cfdf87d3d38
Imran Khan <imrankohistani65@gmail.com>
2025-05-28 17:11:43 -0400

    update thisngd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 const __dirname = path.resolve()
-app.use(cors()); // Add this if calling from frontend on another port
+app.use(cors());
 
 // Put your actual token here or use dotenv
 const BRAWL_API_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQ2Y2Y0ZTJjLWE4MWEtNDFjZS05NTU1LWFmMjYwYjI0MTM0YyIsImlhdCI6MTc0ODIxNzY3OCwic3ViIjoiZGV2ZWxvcGVyLzE5ZjEwOTg1LWFjZGYtYjU2YS1jMjJjLWUyYzE3NDIyMGVmMyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTk4LjU0LjExNC4xMiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.LzUQjEpsdkty9sy4AMIZgfBK_RiCc0dEhjoJ09zLpp91pWO9cGsjhwKUz5dk0kSG8i4itxDi6H_KZyn_uS9GVA';
